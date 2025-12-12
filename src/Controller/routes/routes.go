fd1f988a364a2a6207c13ce7282efa599318632b
sofia <sofiafamorim@gmail.com>
2025-12-12 17:41:35 -0300

    feat: terminando os routes e controllers

--- src/Controller/routes/routes.go
+++ src/Controller/routes/routes.go
@@ -1,13 +1,17 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	controller "Crud-go/src/Controller"
+
+	"github.com/gin-gonic/gin"
+)
 
 func InitRoutes(r*gin.RouterGroup){
 
-	r.GET("/getUserById/:userId")
-	r.GET("/getUserByEmail/:userEmail")
-	r.POST("/createUser/")
-	r.PUT("/updateUser/:userId")
+	r.GET("/getUserById/:userId", controller.FindUserById)
+	r.GET("/getUserByEmail/:userEmail", controller.FindUserByEmail)
+	r.POST("/createUser/", controller.CreateUser)
+	r.PUT("/updateUser/:userId", controller.UpdateUser)
 	r.DELETE("/deleteUser/:userId")
 
 }
