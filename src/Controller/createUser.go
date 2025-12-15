43096235d463f02c4714d745b705e3e46ab4b64d
sofia <sofiafamorim@gmail.com>
2025-12-15 14:30:18 -0300

    feat: Criando objeto de request, para ter  os objetos de entrada e saida de requisição

--- src/Controller/createUser.go
+++ src/Controller/createUser.go
@@ -1,12 +1,25 @@
 package controller
 
 import (
+
+	"fmt"
 	"Crud-go/src/configuration/rest_err"
+	"Crud-go/src/controller/model/request"
 
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context){
-	err := rest_err.NewBadRequestError("Voce chamou a rota de forma errada")
-	c.JSON(err.Code, err)
+	
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(
+			fmt.Sprintf("there are some incorrect filds, error=%s", err.Error))
+
+			c.JSON(restErr.Code, restErr)
+			return
+	}
+
+	fmt.Println(userRequest)
 }
